0edc7b9bd74e0817e5b402c78e9601dd25e6603e
Sevil116 <156057402+Sevil116@users.noreply.github.com>
2025-07-08 16:48:53 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+gsap.from(".navbar", {delay: 0.7, duration: 3, x:-200, ease: "bounce", opacity: 0})
+
+gsap.from(".carousel", {delay:0.7, duration: 3, y: 200, ease: "power2.out", opacity: 0})
+
+gsap.from(".nav-item", {delay: 2, duration: 3, x:200, ease: "power2.inOut", opacity: 0})
+
+gsap.from(".navbar-brand", {opacity: 0, delay: 3, duration: 1, rotation: 720})
